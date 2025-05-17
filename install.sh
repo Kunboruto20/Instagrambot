9eee3ae8ea414c5fbd12713d30dcb11773c382ed
gyovannyvpn123 <mdanut159@gmail.com>
2025-05-18 02:24:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
-pkg update -y && pkg upgrade -y
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Actualizare pachete
+pkg update -y
+pkg upgrade -y
+
+# Instalare dependențe necesare
 pkg install -y nodejs git
-npm install instagram-private-api
-npm install --production
-node index.cjs
+
+# Clonare repository
+git clone https://github.com/gyovannyvpn123/Instagrambot.git
+cd Instagrambot
+
+# Instalare pachete Node.js
+npm install
+
+# Pornire automată a botului
+npm start
