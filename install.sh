6db1992693b5ef55dcb79a5087447dd701b696cc
gyovannyvpn123 <mdanut159@gmail.com>
2025-05-18 02:16:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-#!/data/data/com.termux/files/usr/bin/bash
-echo "=== Instalare Instagram Bot Boruto ==="
 pkg update -y && pkg upgrade -y
 pkg install -y nodejs git
 npm install instagram-private-api
